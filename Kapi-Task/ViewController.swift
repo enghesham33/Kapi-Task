31cd4b425622c0350bf20c42f3778a52fa1f96f4
Hesham Donia <135174044+hkhaled1992@users.noreply.github.com>
2023-11-27 12:53:09 +0200

    initial commit

---
+++ Kapi-Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kapi-Task
+//
+//  Created by Hesham Donia on 27/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
